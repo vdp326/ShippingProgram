a6f6dc453caba474adadabe971e8896bb56130ea
Vu Pham <vupham326@gmail.com>
2019-10-11 11:51:15 -0700

    Shipping Program

---
+++ shipment2/MainWindow.xaml.cs
@@ -0,0 +1,52 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace shipment2
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+
+        private void button_Click(object sender, RoutedEventArgs e)
+        {
+            try
+            {
+
+                // This will declare fields with default values;
+                int carton = 0, items = 0, total = 0;
+
+                // this will convert input values into int and calculate total
+                int.TryParse(textBox.Text, out carton);
+                int.TryParse(textBox1.Text, out items);
+                total = carton * items;
+
+                // this will shows value in Total textbox 
+                textBox2.Text = total.ToString();
+
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show(ex.Message.ToString());
+            }
+
+        }
+    }
+}
